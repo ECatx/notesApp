d29bf2ffb1f7df4982e4fd92100154b1f69eebf7
Eric Castellanos <eric@ericcastellanos.com>
2021-04-15 19:52:25 -0500

    updated to add and save notes

--- src/components/NotesForm.vue
+++ src/components/NotesForm.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="absolute inset-0 w-screen h-screen bg-green-900  opacity-95 flex justify-center items-center">
-        <button @click="showToggle" class="text-white absolute top-3 left-3"><mdi:close-outline class="text-4xl"/>        
+        <button @click="reset" class="text-white absolute top-3 left-3"><mdi:close-outline class="text-4xl"/>        
         </button>
         <form @submit.prevent class="space-y-4">
             <div class="flex flex-col">
                 <label for="title" class="text-white mb-1">Title</label>
-                <input type="text" name="title" class="rounded-md py-2 px-3" placeholder="Title">
+                <input type="text" name="title" class="rounded-md py-2 px-3" placeholder="Title" v-model="newNote.title">
             </div>
             <div class="flex flex-col">
                 <label for="content" class="text-white mb-1">Content</label>
-                <input type="text" name="content" class="rounded-md py-2 px-3" placeholder="Content">
-            </div>
-            <div>
-                <button class="w-full bg-purple-800 text-white p-3 rounded-md mt-5 hover:bg-violet-600 shadow-lg">
-                        Add
+                <input type="text" name="content" class="rounded-md py-2 px-3" placeholder="Content" v-model="newNote.content">
+                <button v-if="noteEdited" class="w-full bg-yellow-800 text-white p-3 rounded-md mt-5 hover:bg-violet-600 shadow-lg" @click="updateNote">
+                    Update
+                </button>
+                <button v-else class="w-full bg-purple-800 text-white p-3 rounded-md mt-5 hover:bg-violet-600 shadow-lg" @click="createNote">
+                    Add
                 </button>
             </div>
         </form>
@@ -21,5 +22,39 @@
 </template>
 
 <script setup>
-import {showToggle} from '../helpers/useNote'
+import {showToggle, add, save,noteEdited} from '../helpers/useNote'
+import {reactive, defineEmit, onMounted} from 'vue'
+
+const emit = defineEmit(['added', 'saved'])
+
+
+const newNote = reactive({
+    title: '',
+    content: '',
+})
+
+onMounted(() => {
+    if (noteEdited.value){
+        newNote.title = noteEdited.value.title
+        newNote.content = noteEdited.value.content
+    }
+})
+
+const reset = event => {
+    showToggle()
+    newNote.title= ''
+    newNote.content= ''
+    noteEdited.value = null
+    emit(event)
+}
+
+const createNote = async () => {
+    await add(newNote)
+    reset('added')
+}
+
+const updateNote = async () => {
+    await save({ id:noteEdited.value.id, ...newNote})
+    reset('saved')
+}
 </script>
